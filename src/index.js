10c3e3e525fd3c74acc200909f1548c7bdabccc1
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-04-18 09:19:13 -0500

    added notes to main index.js file, what the import statments are bringing in etc

--- src/index.js
+++ src/index.js
@@ -4,6 +4,25 @@ import "./styles.css";
 
 import App from "./App";
 
+// Click on the file labeled index.js in
+//  the Files section of CodeSandbox. 
+ 
+//  You won’t be editing this file during
+//   the tutorial but it is the bridge
+//    between the component you created in the 
+// App.js file and the web browser.
+
+// Lines 1-5 brings all the necessary pieces together:
+
+// - React
+// - React’s library to talk to web browsers (React DOM)
+// - the styles for your components
+// - the component you created in App.js.
+
+// The remainder of the file brings all the
+//  pieces together and injects the final
+//  product into index.html in the public folder.
+
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
