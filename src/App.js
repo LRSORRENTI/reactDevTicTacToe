b1b50310b14433e42af564a68fabe5e2cfebc540
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-04-18 09:39:59 -0500

    following the docs, I'm a little confused by the set-up, but clarity will come when I'm finished, I hope

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@
 //  Let’s look at the component
 //  line by line to see what’s going on:
 
-export default function Square() {
-  return <button className="square">X</button>;
-}
+// export default function Square() {
+//   return <button className="square">X</button>;
+// }
 
 // The first line defines a function called Square. 
 // The export JavaScript keyword makes this 
@@ -35,3 +35,79 @@ export default function Square() {
 //      element to indicate that any following 
 //      content shouldn’t
 //  be placed inside the button.
+
+// Building the board 
+// Let’s get back to App.js. 
+// This is where you’ll spend
+//  the rest of the tutorial.
+
+// Currently the board is only a 
+// single square, but you need nine! 
+// If you just try and copy
+//  paste your square to make 
+//  two squares like this:
+
+// export default function Square() {
+//   return <button className="square">X</button><button className="square">X</button>;
+// }
+
+// You’ll get this error:
+
+// Console
+// /src/App.js: Adjacent JSX 
+// elements must be wrapped in an 
+// enclosing tag. Did you want a 
+// JSX fragment <>...</>?
+
+// React components need to return a
+//  single JSX element and not multiple
+//   adjacent JSX elements like two buttons.
+  
+//   To fix this you can use fragments 
+//   (<> and </>) to wrap multiple 
+//   adjacent JSX elements like this:
+
+// export default function Square() {
+//   return (
+//     <>
+//       <button className="square">X</button>
+//       <button className="square">X</button>
+//     </>
+//   );
+// }
+
+// Oh no! The squares are all in a single line, 
+// not in a grid like you need for our board.
+
+// To fix this you’ll need to group your 
+// squares into rows with divs and add some
+//  CSS classes. While you’re at it, 
+//  you’ll give each square a number
+//   to make sure you know where each square 
+//   is displayed.
+
+// In the App.js file, update the
+//  Square component to look like this:
+
+export default function Board() {
+  return (
+    <>
+    <h1>TicTacToe</h1>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
+  );
+}
