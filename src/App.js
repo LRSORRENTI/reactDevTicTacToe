08dbe2e384654ea51aa075fe4443a3f2e56e23c6
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-04-18 09:01:45 -0500

    added notes inside the App.js file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,37 @@
+// The code in App.js creates a component. 
+// In React, a component is a piece of reusable
+//  code that represents a part of a user interface.
+ 
+//  Components are used to render, manage, 
+//  and update the UI elements in your application. 
+ 
+//  Let’s look at the component
+//  line by line to see what’s going on:
+
 export default function Square() {
   return <button className="square">X</button>;
 }
+
+// The first line defines a function called Square. 
+// The export JavaScript keyword makes this 
+// function accessible outside of this file. 
+
+// The default keyword tells other files using your
+//  code that it’s the main function in your file
+
+// The second line returns a button.
+//  The return JavaScript keyword means 
+//  whatever comes after is returned as 
+//  a value to the caller of the function.
+ 
+//  <button> is a JSX element. 
+//   A JSX element is a combination of 
+//   JavaScript code and HTML tags that
+//    describes what you’d like to display. 
+//    className="square" is a button property
+//     or prop that tells CSS how to style the button.
+//      X is the text displayed inside of the 
+//      button and </button> closes the JSX 
+//      element to indicate that any following 
+//      content shouldn’t
+//  be placed inside the button.
